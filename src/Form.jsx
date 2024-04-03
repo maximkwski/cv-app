4bf4dfc6706aa2e62de6cf1d5c50626114a68716
Maksym <makkie.makowski@gmail.com>
2024-04-03 17:18:52 -0400

    adjust form fields, add styles

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,75 +1,93 @@
+import './styles/Form.css';
+
 export default function Form({ fullName, onChange}) {
 
     return (
-        <form className='cv-form'>
+        <form className='cv-form container'>
             <section className='personal'>
-            <div className="form-row">
-                <label htmlFor="name">Full name</label>
-                <input type="text" name="name" id="name" onChange={onChange} value={fullName}/>
-            </div>
-            <div className="form-row">
-                <label htmlFor="phone">Contact number</label>
-                <input type="tel" name="phone" id="phone" />
-            </div>
-            <div className="form-row">
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" />
-            </div>
+                <h2>Personal Info</h2>
+                <div className="form-row">
+                        <label htmlFor="name">Full name</label><br />
+                        <input type="text" name="name" id="name" onChange={onChange} value={fullName}/>
+                </div>        
+                <div className="form-row">
+                    <label htmlFor="title">Job title</label><br />
+                    <input type="text" name="title" id="title" />
+                </div>
+                <div className="form-row">
+                        <label htmlFor="email">Email</label><br />
+                        <input type="email" name="email" id="email" />
+                </div>
+                <div className="form-row">
+                    <label htmlFor="phone">Contact number</label><br />
+                    <input type="tel" name="phone" id="phone" />
+                </div>       
+                <div className="form-row">
+                    <label htmlFor="dob">Date of birth</label><br />
+                    <input type="text" name="dob" id="dob" />
+                </div>
             </section>
 
             <section className='education'>
-            <div className="form-row">
-                <label htmlFor="study">Course/Degree/Class</label>
-                <input type="text" name="study" id="study" />
-            </div>
-            <div className="form-row">
-                <label htmlFor="school">School/Univercity</label>
-                <input type="text" name="school" id="school" />
-            </div>
-            <div className="form-row">
-                <label htmlFor="location">Location</label>
-                <input type="text" name="location" id="location" placeholder='ex: Miami, Florida'/>
-            </div>
-            <div className="form-row">
-                <label htmlFor="grad-year">Year of graduation</label>
-                <input type="text" name="grad-year" id="grad-year" />
-            </div>
-            <button className='btn'>Add</button>
-            <button className='btn'>Delete</button>
+                <h2>Education</h2>
+                <div className="form-row">
+                    <label htmlFor="study">Course/Degree/Class</label><br />
+                    <input type="text" name="study" id="study" />
+                </div>
+                <div className="form-row">
+                    <label htmlFor="school">School/Univercity</label><br />
+                    <input type="text" name="school" id="school" />
+                </div>
+                <div className="form-row">
+                    <label htmlFor="location">Location</label><br />
+                    <input type="text" name="location" id="location" placeholder='ex: Miami, Florida'/>
+                </div>
+                <div className="form-row">
+                    <label htmlFor="grad-year">Year of graduation</label><br />
+                    <input type="text" name="grad-year" id="grad-year" />
+                </div>
+                <div className="control-panel">
+                    <button className='btn'>Add another</button>
+                    <button className='btn'>Delete</button>
+                </div>
             </section>
 
             <section className='experience'>
-            <div className="form-row">
-                <label htmlFor="company">Company name</label>
-                <input type="text" name="company" id="company" />
-            </div>
-            <div className="form-row">
-                <label htmlFor="job-title">Job title</label>
-                <input type="text" name="job-title" id="job-title" />
-            </div>
-            <div className="form-row">
-                <label htmlFor="job-desc">Main responsibilities</label>
-                <textarea  name="job-desc" id="job-desc" cols="30" rows="5" />
-            </div>
-            <div className="form-row">
-                <label htmlFor="job-start">From</label>
-                <input type="text" name="job-start" id="job-start" />
-            </div>
-            <div className="form-row">
-                <label htmlFor="job-end">To</label>
-                <input type="text" name="job-end" id="job-end" />
-            </div>
-            <button className='btn'>Add</button>
-            <button className='btn'>Delete</button>
+                <h2>Work History</h2>
+                <div className="form-row">
+                    <label htmlFor="company">Company name</label><br />
+                    <input type="text" name="company" id="company" />
+                </div>
+                <div className="form-row">
+                    <label htmlFor="job-title">Job title</label><br />
+                    <input type="text" name="job-title" id="job-title" />
+                </div>
+                <div className="form-row">
+                    <label htmlFor="job-desc">Main responsibilities</label><br />
+                    <textarea  name="job-desc" id="job-desc" cols="30" rows="5" />
+                </div>
+                <div className="form-row">
+                    <label htmlFor="job-start">From</label><br />
+                    <input type="text" name="job-start" id="job-start" />
+                </div>
+                <div className="form-row">
+                    <label htmlFor="job-end">To</label><br />
+                    <input type="text" name="job-end" id="job-end" />
+                </div>
+                <div className="control-panel">
+                    <button className='btn'>Add another</button>
+                    <button className='btn'>Delete</button>
+                </div>
             </section>
             <section className='skills'>
-            <label htmlFor="skills">Skills/Languages</label>
-            <textarea name="skills" id="skills" cols="30" rows="5"></textarea>
-            
+                <h2>Skills</h2>
+                <div className="form-row">
+                    <textarea name="skills" id="skills" cols="30" rows="5" placeholder='ex: Html, Javascript, CSS...'></textarea>
+                </div>
             </section>
             <div className="control-panel">
-            <button type="reset" className='btn'>reset</button>
-            <button type="submit" className='btn'>submit</button>
+                <button type="reset" className='btn'>reset</button>
+                <button type="submit" className='btn'>submit</button>
             </div>
       </form>
     )
